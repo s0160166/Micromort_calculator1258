e88dc42f136bbdd21d8b27613b7957e9e7f16aec
Kuchugurov <79853342+s0160166@users.noreply.github.com>
2022-12-19 21:01:50 +0300

    Delete Курсач directory

--- Курсач/micromort/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.index, name='home'),
-    path('create', views.create, name='create'),
-    path('results', views.results, name='results'),
-]
